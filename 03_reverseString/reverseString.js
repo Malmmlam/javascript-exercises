cd85bea9fbec476d6bdad867021f7cdd57b6d4b3
malmmlam <malmmlam@outlook.com>
2022-01-11 13:59:00 +0100

    complete exercise 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
+    let returnStr = "";
+    
+    for(let i = str.length - 1; i >= 0; i--) {
+        returnStr += str[i];
+    }
 
+    return returnStr;
 };
 
 // Do not edit below this line
