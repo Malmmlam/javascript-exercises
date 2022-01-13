d5d9f160a9c6ceec7118f995733ef0aa2ced3ecf
malmmlam <malmmlam@outlook.com>
2022-01-13 15:12:52 +0100

    complete sumAll exercise

---
+++ 05_sumAll/SCRATCH.md
@@ -0,0 +1,9 @@
+Plan
+    
+Restate
+    Create a program that returns the sum of all the numbers between two given numbers and the given numbers themselves.
+
+Divide
+    Create a program that returns the sum of two numbers. 
+    Create a program with a loop that adds the numbers between the two together. 
+    Add a feature to the program that sorts the variables before addition. 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let returnNum = 0;
+    let numArray = [num1, num2];
 
+    for(let i = 0; i < numArray.length; i++) {
+        if(numArray[i] < 0 || typeof numArray[i] != "number") {
+            return "ERROR";
+        }
+    }
+
+    numArray.sort(function(a, b) 
+        {return a - b});
+    
+    for(let i = 0; i <= numArray[1]; i++) {
+        returnNum += i;
+    }
+    
+    return returnNum;
 };
 
 // Do not edit below this line
