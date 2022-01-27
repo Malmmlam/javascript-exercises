69920ac414ed9e184a6a278079d03f9b95e191e6
Josh <malmmlam@outlook.com>
2022-01-27 14:15:40 +0100

    complete exercise 12, find the oldest

---
+++ 12_findTheOldest/SCRATCH.md
@@ -0,0 +1,5 @@
+Plan
+    Restate
+        given a list of people and at least their birth dates return the person that has lived the greatest number of years 
+    Divide
+        Write a function that returns the person that was born earliest when given a list of people. 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    for(let person of people) {
+        if(person["yearOfDeath"] === undefined) {
+            person["yearOfDeath"] = new Date().getFullYear();
+        }
+    }
+
+    let oldest = people.reduce((oldest, person) => 
+        person.yearOfDeath - person.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth ? 
+        person : oldest);
+
+    return oldest;
 };
+
+    // non-ternary version of final function, for reference. 
+    // let oldest = people.reduce((oldest, person) => {
+    //     if(person.yearOfDeath - person.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth) {
+    //         oldest = person;
+    //     }
+    //     return oldest;
+    // });
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
